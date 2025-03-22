3d36b7d2481de9201a09167bae84c97ca72921e3
jj <syeonie0526@gmail.com>
2025-03-22 14:22:11 +0900

    chore: user 도메인 초기 설계

---
+++ src/main/java/com/blog/domain/user/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.blog.domain.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
