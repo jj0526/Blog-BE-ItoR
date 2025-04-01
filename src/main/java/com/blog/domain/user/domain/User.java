1ec57d8e5b4844fee0e5957db7e0284aba2b88e6
jj <syeonie0526@gmail.com>
2025-04-01 17:23:57 +0900

    refactor: 생일을 LocalDate 타입으로 통합

--- src/main/java/com/blog/domain/user/domain/User.java
+++ src/main/java/com/blog/domain/user/domain/User.java
@@ -1,6 +1,6 @@
 package com.blog.domain.user.domain;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import com.blog.domain.user.dto.UserSaveDTO;
 import com.blog.global.common.entity.BaseEntity;
@@ -11,8 +11,8 @@ public class User extends BaseEntity {
 	String email;
 	String password;
 	String name;
-	String nickName;
-	Date birthDate;
+	String nickname;
+	LocalDate birthDate;
 	String profileImageUrl;
 	String introduction;
 
@@ -40,9 +40,31 @@ public class User extends BaseEntity {
 	}
 
 
-	public static User fromDTO(String email, String password, String name) {
-		return new User(email, password, name);
+	public User (String email, String password, String name, String nickname,
+		String introduction, LocalDate birthDate, String profileImageUrl, Long kakaoId) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.kakaoId = kakaoId;
+		this.nickname = nickname;
+		this.introduction = introduction;
+		this.birthDate = birthDate;
+		this.profileImageUrl = profileImageUrl;
 	}
+
+	public static User fromDTO(UserSaveDTO userSaveDTO) {
+		return new User(
+			userSaveDTO.getEmail(),
+			userSaveDTO.getPassword(),
+			userSaveDTO.getName(),
+			userSaveDTO.getNickname(),
+			userSaveDTO.getIntroduction(),
+			userSaveDTO.getBirthDate(),
+			userSaveDTO.getProfileImageUrl(),
+			null  // kakaoId는 UserSaveDTO에 없으므로 null로 설정
+		);
+	}
+
 
 	public Long getId() {
 		return id;
@@ -59,6 +81,27 @@ public class User extends BaseEntity {
 	public String getPassword() {
 		return password;
 	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public String getProfileImageUrl() {
+		return profileImageUrl;
+	}
+
+	public String getIntroduction() {
+		return introduction;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
 
 	public Long getKakaoId(){ return kakaoId; }
 
