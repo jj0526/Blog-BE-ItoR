f82e0e5668572e55f68b188e978b455ac360723f
jj <syeonie0526@gmail.com>
2025-04-01 15:45:47 +0900

    feat: 카카오 로그인 서비스 및 유틸 클래스 구현

--- src/main/java/com/blog/global/auth/login/controller/LoginController.java
+++ src/main/java/com/blog/global/auth/login/controller/LoginController.java
@@ -1,28 +1,41 @@
 package com.blog.global.auth.login.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.blog.global.auth.login.dto.AuthenticationResultDTO;
-import com.blog.global.auth.login.dto.LoginRequestDTO;
-import com.blog.global.auth.login.service.LoginService;
+import com.blog.global.auth.login.kakao.service.KakaoLoginService;
+import com.blog.global.auth.login.standard.dto.AuthenticationResultDTO;
+import com.blog.global.auth.login.standard.dto.LoginRequestDTO;
+import com.blog.global.auth.login.standard.service.LoginService;
 import com.blog.global.common.response.CommonResponse;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController
-@RequestMapping("/login")
+@RequestMapping("")
 public class LoginController {
 	private final LoginService loginService;
+	private final KakaoLoginService kakaoLoginService;
 
-	public LoginController(LoginService loginService) {
+	public LoginController(LoginService loginService, KakaoLoginService kakaoLoginService) {
 		this.loginService = loginService;
+		this.kakaoLoginService = kakaoLoginService;
 	}
 
-	@PostMapping
+	@GetMapping("/login")
 	public CommonResponse<AuthenticationResultDTO> login(@RequestBody LoginRequestDTO request){
 		AuthenticationResultDTO authResult = loginService.authenticate(request);
 
 		return CommonResponse.createSuccess(authResult);
 	}
+	@GetMapping("/auth/kakao/login")
+	public CommonResponse<AuthenticationResultDTO> kakaoLogin(@RequestParam("code") String accessCode,
+		HttpServletResponse httpServletResponse){
+		return CommonResponse.createSuccess(kakaoLoginService.oAuthLogin(accessCode, httpServletResponse));
+	}
+
 }
---
+++ src/main/java/com/blog/global/auth/login/kakao/util/KakaoUtil.java
@@ -0,0 +1,83 @@
+package com.blog.global.auth.login.kakao.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import com.blog.global.auth.login.exception.KakaoParsingException;
+import com.blog.global.auth.login.kakao.dto.KakaoDTO;
+import com.blog.global.auth.login.kakao.dto.KakaoProfile;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class KakaoUtil {
+	@Value("${blog.kakao.auth.client}")
+	private String client;
+	@Value("${blog.kakao.auth.redirect}")
+	private String redirect;
+
+	public KakaoDTO requestToken(String accessCode) {
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", client);
+		params.add("redirect_url", redirect);
+		params.add("code", accessCode);
+
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(
+			"https://kauth.kakao.com/oauth/token",
+			HttpMethod.POST,
+			kakaoTokenRequest,
+			String.class);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		KakaoDTO oAuthToken = null;
+
+		try {
+			oAuthToken = objectMapper.readValue(response.getBody(), KakaoDTO.class);
+		} catch (JsonProcessingException e) {
+			throw new KakaoParsingException();
+		}
+		return oAuthToken;
+	}
+
+	public KakaoProfile requestProfile(KakaoDTO oAuthToken) {
+		RestTemplate restTemplate2 = new RestTemplate();
+		HttpHeaders headers2 = new HttpHeaders();
+
+		headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		headers2.add("Authorization", "Bearer " + oAuthToken.getAccessToken());
+
+		HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers2);
+
+		ResponseEntity<String> response2 = restTemplate2.exchange(
+			"https://kapi.kakao.com/v2/user/me",
+			HttpMethod.GET,
+			kakaoProfileRequest,
+			String.class);
+
+		KakaoProfile kakaoProfile = null;
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		try {
+			kakaoProfile = objectMapper.readValue(response2.getBody(), KakaoProfile.class);
+		} catch (JsonProcessingException e) {
+
+			throw new KakaoParsingException();
+		}
+
+		return kakaoProfile;
+	}
+}
