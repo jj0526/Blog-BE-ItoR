cd6cf3af2dde0daa67e5715359fe0da3e4d38b02
jj <syeonie0526@gmail.com>
2025-04-08 14:02:58 +0900

    refactor: ContentDTO.Save의 contentType enum 검증 로직 추가

--- src/main/java/com/blog/domain/post/application/dto/ContentDTO.java
+++ src/main/java/com/blog/domain/post/application/dto/ContentDTO.java
@@ -7,7 +7,8 @@ import jakarta.validation.constraints.NotNull;
 public class ContentDTO {
 	public record Save(
 		@NotBlank(message = "내용을 입력해주세요.") String contentData,
-		@NotNull ContentType contentType
+		@NotNull(message = "contentType은 null일 수 없으며, 'BODY' 또는 'IMAGE'여야 합니다.")
+		ContentType contentType
 	){}
 
 	public record Response(
