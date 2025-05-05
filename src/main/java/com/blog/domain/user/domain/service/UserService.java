3ad15169080700abc5de0da2f85df16bc0347845
jj <syeonie0526@gmail.com>
2025-05-05 16:21:30 +0900

    feat: UserMapper 구현 및 유저 도메인에 적용

--- src/main/java/com/blog/domain/user/domain/service/UserService.java
+++ src/main/java/com/blog/domain/user/domain/service/UserService.java
@@ -1,8 +1,8 @@
 package com.blog.domain.user.domain.service;
 
 import org.springframework.stereotype.Service;
-
 import com.blog.domain.user.application.dto.UserDTO;
+import com.blog.domain.user.application.mapper.UserMapper;
 import com.blog.domain.user.domain.entity.User;
 import com.blog.domain.user.application.exception.UserAlreadyExistsException;
 import com.blog.domain.user.application.exception.UserNotFoundException;
@@ -14,30 +14,26 @@ public class UserService {
 
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final UserMapper userMapper;
 
-	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, UserMapper userMapper) {
 		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
+		this.userMapper = userMapper;
 	}
 
 	public void save(UserDTO.Save dto) {
 		if(userRepository.findByEmail(dto.email()).isPresent()){
 			throw new UserAlreadyExistsException();
 		}
-		User user = User.fromDTO(dto.email(), passwordEncoder.encrypt(dto.email(), dto.password()),
-			dto.name(),
-			dto.nickname(),
-			dto.introduction(),
-			dto.birthDate(),
-			dto.profileImageUrl(),
-			0L);	// kakaoId는 일반 로그인 유저가 사용하지 않으므로 0으로 설정);
+		User user = userMapper.fromDTO(dto, passwordEncoder);
 
 		userRepository.save(user);
 	}
 
 	public UserDTO.Response getMyInfo(long userId) {
 		User user = userRepository.find(userId).orElseThrow(UserNotFoundException::new);
-		return new UserDTO.Response(user);
+		return userMapper.toResponse(user);
 	}
 
 	public void update(long userId, UserDTO.Update dto) {
